19e81a1f98d6f8e5ed1bf74039f5c7fa532e9de7
siwin-anto <121216316+siwin-anto@users.noreply.github.com>
2023-05-01 14:47:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,39 +2,76 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
-export default class App extends Component {
+// export default class App extends Component {
 
-        state = { advice : ''};
+//         state = { advice : ''};
 
-        componentDidMount(){
-         this.fetchData();
-        }
+//         componentDidMount(){
+//          this.fetchData();
+//         }
         
-        fetchData = () => {
-            axios.get('https://api.adviceslip.com/advice')  
-                .then( re => {
-                    const {advice} = re.data.slip
-                        this.setState({advice});
-                })
-                .catch(error =>{
-                        console.log(error);
-                });
-        }
+//         fetchData = () => {
+//             axios.get('https://api.adviceslip.com/advice')  
+//                 .then( re => {
+//                     const {advice} = re.data.slip
+//                         this.setState({advice});
+//                 })
+//                 .catch(error =>{
+//                         console.log(error);
+//                 });
+//         }
 
-    render() {
-        return (
-            <div className='app'>
-                <div className='card'>  
-                    <h1 className='heading'>{this.state.advice}</h1>
-                    <button className='button' onClick={this.fetchData}> 
-                        <span>
-                            Click for advice
-                        </span>
-                    </button>
-                </div>
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div className='app'>
+//                 <div className='card'>  
+//                     <h1 className='heading'>{this.state.advice}</h1>
+//                     <button className='button' onClick={this.fetchData}> 
+//                         <span>
+//                             Click for advice
+//                         </span>
+//                     </button>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+ const App = () => {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((re) => {
+        const { advice } = re.data.slip;
+        setAdvice(advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={() =>fetchData()}>
+          <span>Click for advice</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App
 
 
